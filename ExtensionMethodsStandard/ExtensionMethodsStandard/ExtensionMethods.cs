cfbe28cc465cb8b08df08e97905b904a79d5d658
Johnny Dockhorn <johnnyerick111@hotmail.com>
2020-03-25 16:17:11 -0300

    First Commit

---
+++ ExtensionMethodsStandard/ExtensionMethodsStandard/ExtensionMethods.cs
@@ -0,0 +1,10 @@
+﻿namespace ExtensionMethodsStandard
+{
+    public static class ExtensionMethods
+    {
+        public static bool IsNullOrEmpty(this string word) => string.IsNullOrEmpty(word);
+        public static bool IsNullOrWhiteSpace(this string word) => string.IsNullOrWhiteSpace(word);
+        public static bool IsNullOrEmpty(this object obj) => obj == null ? false : true;
+
+    }
+}
